32339025e582b8f337648e455ba93081d5cd9f81
gyong <gitadmin@gybeongotan.dev>
2024-09-09 06:35:28 +0800

    expose httpz and pg lib

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
-pub usingnamespace @import("httpz");
 pub const MainModule = @import("main.zig").MainModule;
 pub const AuthToken = @import("auth-token.zig");
 pub const TypeUtils = @import("type-utils.zig");
+pub const httpz = @import("httpz");
+pub const pg = @import("pg");
