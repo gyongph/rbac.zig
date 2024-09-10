51a0bd57ad3444b50f2397f4d4deb859d9d9d0f3
gyong <gitadmin@gybeongotan.dev>
2024-09-10 15:04:30 +0800

    delete auth-token.zig

--- src/lib.zig
+++
@@ -1,5 +0,0 @@
-pub const pg = @import("pg");
-pub const httpz = @import("httpz");
-pub const MainModule = @import("main.zig").MainModule;
-pub const AuthToken = @import("auth-token.zig");
-pub const TypeUtils = @import("type-utils.zig");
--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,4 @@
 pub const MainModule = @import("main.zig").MainModule;
-pub const AuthToken = @import("auth-token.zig");
 pub const TypeUtils = @import("type-utils.zig");
 pub const pg = @import("pg.zig");
 pub const httpz = @import("httpz.zig");
