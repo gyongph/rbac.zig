38a1fba397d9853c034e4b6e16572c2cb282f4a2
gyong <gitadmin@gybeongotan.dev>
2024-10-12 00:08:32 +0800

    add improvement

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,6 @@
 pub const MainModule = @import("main.zig").MainModule;
+pub const TokenPayload = @import("main.zig").TokenPayload();
 pub const TypeUtils = @import("type-utils.zig");
 pub const pg = @import("pg.zig");
 pub const httpz = @import("httpz.zig");
+pub const Token = @import("utils/token.zig");
