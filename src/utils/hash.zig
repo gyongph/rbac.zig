0735107c08ed2434f49a7134453ffab08aa9392f
gyong <gitadmin@gybeongotan.dev>
2024-10-26 01:08:07 +0800

    test: fix hash result

--- src/utils/hash.zig
+++ src/utils/hash.zig
@@ -11,15 +11,15 @@ pub fn hash(input: []const u8, dest: *[64]u8) !void {
 test "Sha512 Hash" {
     const allocator = std.testing.allocator;
     const input = "2gIJOg9EkqEMX01/Q3GYrYX4M1iRb4VKKF85gZ";
-    const output_base64 = "FCQCOXOZpzx+HU8f7nPY2AHdciCPjs9ErRx8tCtAfSj9DHSRYUTffOYspz81L3rMqXO1ukOiSnqAaEb9ylDBZg==";
+    const output_base64 = "FCQCOXOZpzx-HU8f7nPY2AHdciCPjs9ErRx8tCtAfSj9DHSRYUTffOYspz81L3rMqXO1ukOiSnqAaEb9ylDBZg";
     var result_hash_buf = [_]u8{undefined} ** 64;
     try hash(input, &result_hash_buf);
     const result_base64 = try base64.encode(allocator, &result_hash_buf);
     defer allocator.free(result_base64);
     const equal = std.mem.eql(u8, output_base64, result_base64);
     if (!equal) {
-        std.debug.print("{any}\n", .{result_hash_buf});
-        std.debug.print("{s}\n", .{result_base64});
+        std.debug.print("result buff{any}\n", .{result_hash_buf});
+        std.debug.print("result {s}\n", .{result_base64});
     }
     try std.testing.expect(equal);
 }
